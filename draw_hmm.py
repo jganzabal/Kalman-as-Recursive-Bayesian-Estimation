e83365237080351a693bb29a89a714c7c245f10c
Julian Ganzabal <jganzabal@gmail.com>
2017-05-19 04:41:14 -0300

    Initial commit

---
+++ draw_hmm.py
@@ -0,0 +1,72 @@
+from matplotlib.patches import Ellipse, Arc, ConnectionPatch, ConnectionStyle, FancyArrow
+from matplotlib import pyplot as plt
+delta = 2
+arrow_width = 0.03
+
+def draw_state(state=0, state_label = 0):
+    radius = 0.5
+    x = state*delta
+    y = 0
+    pos_circle = plt.Circle((x, y), radius=radius, fill=False, color='b', lw=2)
+    plt.gca().add_patch(pos_circle)
+    plt.text(x, y, '$S_{%s}$'%(str(state_label)), fontsize=12, horizontalalignment='center', verticalalignment='center')
+
+
+def draw_self_transition(state=0, text='0.55'):
+    x_pos = state*delta
+    y_pos = 0.7
+    height = 0.60
+    width = 0.55
+    ellipse = Arc([x_pos,y_pos],width,height,angle=0,theta1=-50, theta2=230.0, color='k', linewidth='1')
+    plt.gca().add_patch(ellipse)
+    arrow2 = FancyArrow(x_pos-0.01, y_pos+height/2, 0.01, -0.002, width=arrow_width, length_includes_head=False, head_width=None, head_length=None, shape='full', overhang=0, head_starts_at_zero=False, color='k')
+    plt.gca().add_patch(arrow2)
+    plt.text(x_pos, y_pos + 1.1*height/2, text, fontsize = 12, horizontalalignment='center', verticalalignment='bottom')
+
+
+def draw_transition(state1=0, state2=1, text=0.1):
+    radius = 0.5
+    x1 = state1*delta + radius
+    diff_state = abs(state2-state1)
+    x2 = delta*diff_state - 2*radius
+    y = 0
+    if diff_state == 1:
+        ellipse = Arc([x1+x2/2,y],x2,0,angle = 0,theta1=0, theta2=180.0,color='k', linewidth='1')
+        plt.text(x1+x2/2, y+ 0.1, text, fontsize = 12, horizontalalignment='center', verticalalignment='bottom')
+        arrow2 = FancyArrow(x1+x2/2-0.01, 0, 0.01, 0, width=arrow_width, length_includes_head=False, head_width=None, head_length=None, shape='full', overhang=0, head_starts_at_zero=False, color='k')
+    else:
+        new_y = -0.2
+        arc_rad = 3.3
+        ellipse = Arc([x1+x2/2,new_y],x2*1.2,arc_rad,angle = 0,theta1=20, theta2=160.0,color='k', linewidth='1')
+        plt.text(x1+x2/2, arc_rad/2+new_y+ 0.1, text, fontsize = 12, horizontalalignment='center', verticalalignment='bottom')
+        arrow2 = FancyArrow(x1+x2/2-0.01, arc_rad/2+new_y, 0.01, 0, width=arrow_width, length_includes_head=False, head_width=None, head_length=None, shape='full', overhang=0, head_starts_at_zero=False, color='k')
+    plt.gca().add_patch(ellipse)
+    plt.gca().add_patch(arrow2)
+    
+def draw_observed(state=0, state_label = 0):
+    radius = 0.5
+    x = state*delta
+    y = -2
+    pos_circle = plt.Circle((x, y), radius=radius, fill=False, color='g', lw=2)
+    plt.gca().add_patch(pos_circle)
+    plt.text(x, y, '$X_{%s}$'%(str(state_label)), fontsize=12, 
+             horizontalalignment='center', verticalalignment='center')
+    arrow = plt.Arrow(x, -radius, 0, -2+2*radius, width=0.25)
+    plt.gca().add_patch(arrow)
+
+def draw_infinite(state1=0, state2=1, text=0.1):
+    radius = 0.5
+    x1 = state1*delta + radius
+    diff_state = abs(state2-state1)
+    x2 = delta*diff_state - 2*radius
+    y = 0
+    ellipse = Arc([x1+x2/2,y],x2,0,angle = 0,theta1=0, theta2=180.0,color='k', linewidth='1',ls='dotted')
+    arrow2 = FancyArrow(x1+x2/2-0.01, 0, 0.01, 0, width=arrow_width, length_includes_head=False, head_width=None, head_length=None, shape='full', overhang=0, head_starts_at_zero=False, color='k')
+    plt.gca().add_patch(ellipse)
+    plt.gca().add_patch(arrow2)
+    
+    y = 0
+    ellipse = Arc([1,y],x2,0,angle = 0,theta1=0, theta2=180.0,color='k', linewidth='1',ls='dotted')
+    plt.gca().add_patch(ellipse)
+    arrow2 = FancyArrow(1-0.01, 0, 0.01, 0, width=arrow_width, length_includes_head=False, head_width=None, head_length=None, shape='full', overhang=0, head_starts_at_zero=False, color='k')
+    plt.gca().add_patch(arrow2)
---
+++ kalman_helper.py
@@ -0,0 +1,83 @@
+
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.ndimage.filters import convolve1d
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+
+def update(mean1, var1, mean2, var2):
+    new_mean = float(var2 * mean1 + var1 * mean2) / (var1 + var2)
+    new_var = 1./(1./var1 + 1./var2)
+    return [new_mean, new_var]
+
+def predict(mean1, var1, mean2, var2):
+    new_mean = mean1 + mean2
+    new_var = var1 + var2
+    return [new_mean, new_var]
+
+def get_observation_distribution(indexes, loc = 1, scale = 2):
+    center = int(int(len(indexes)+1)/2)
+    exponential_dist = np.exp(-abs(indexes-center)/scale)
+    exponential_dist = exponential_dist/exponential_dist.sum()
+    n = center - loc
+    return np.roll(exponential_dist,-n)
+
+def get_next_step_distribution(indexes, scale = 2):
+    exponential_dist = np.exp(-abs(indexes)/scale)
+    exponential_dist = exponential_dist/exponential_dist.sum()
+    return np.roll(exponential_dist,1)
+
+def plot_distribution(indexes, states_probabilities, title = ''):
+    width = 1/1.5
+    plt.figure(figsize=(20, 3))
+    plt.bar(indexes, states_probabilities, width=width)
+    plt.xticks(indexes)
+    plt.title(title)
+    plt.show()
+
+def after_observation_distribution_update(states_distribution, observation_dist):
+    new_distribution = states_distribution * observation_dist
+    new_distribution = new_distribution/new_distribution.sum()
+    return new_distribution
+
+def stochastic_next_step_distribution_prediction(dist1, dist2):
+    center = int(int(len(dist1))/2)
+    return convolve1d(dist1, dist2, mode = 'wrap', origin = -center)
+
+def deterministic_next_step_distribution_prediction(states_distribution):
+    return np.roll(states_distribution,1)
+
+def plot_robot_room(N = 21, loc = 0):
+    pos_radius = 0.8
+    small_radius = 0.1
+    angle = 0
+    N = 21
+    delta_angle = 2*np.pi/N
+    fig, ax = plt.subplots(figsize=(8,8))
+    #plt.figure(figsize=(5,5))
+    im = plt.imread('robot.png')
+    oi = OffsetImage(im, zoom = 0.25)
+    rx = 0
+    ry = 0
+    
+    for i in range(N):
+        x = pos_radius*np.cos(angle)
+        y = pos_radius*np.sin(angle)
+        if (i+1) == loc:
+            box = AnnotationBbox(oi, (x, y), frameon=False)
+            ax.add_artist(box)
+            pos_circle = plt.Circle((x, y), radius=small_radius, fill=True, color = 'y')
+        else:
+            pos_circle = plt.Circle((x, y), radius=small_radius, fill=False, color = 'g')
+        plt.gca().add_patch(pos_circle)
+        angle = angle + delta_angle
+        plt.text(x, y, str(i+1), fontsize = 12, horizontalalignment='center', verticalalignment='center')
+    circle_out = plt.Circle((0, 0), radius=1, fill=False)
+    plt.gca().add_patch(circle_out)
+    circle_in = plt.Circle((0, 0), radius=0.95, fill=False)
+    plt.gca().add_patch(circle_in)
+    circle_inner = plt.Circle((0, 0), radius=0.65, fill=False)
+    plt.gca().add_patch(circle_inner)
+
+    plt.axis('scaled')
+    plt.axis('off')
+    plt.show()
